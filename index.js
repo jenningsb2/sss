652402da0272969c2f1e02c6a100f6deed6292b5
Bailey Jennings <jenningsebailey@gmail.com>
2025-05-04 11:40:07 -0400

    font

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ function processMarkdownFile(filePath) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jacquard+12&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="styles.css">
         <title>${path.basename(cleanName, '.md')}</title>
     </head>
